a7aeb24d448428adc6707f9e64ca64ee823200b8
Bahar-Sarac <117797408+Bahar-Sarac@users.noreply.github.com>
2023-01-07 15:00:06 +0300

    Update and rename homework102.java to heterometriksayilar.java

--- heterometriksayilar.java
+++ heterometriksayilar.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
  *
  * @author Bahar
  */
-public class homework102 {
+public class heterometriksayilar {
 
     /**
      * @param args the command line arguments
@@ -14,10 +14,10 @@ public class homework102 {
     public static void main(String[] args) {
         Scanner input = new Scanner (System.in);
         
-        System.out.println("Terim sayısı: ");
-        int terim_sayisi = input.nextInt();
+        System.out.println("Terim sayısı giriniz: ");
+        int terim = input.nextInt();
         System.out.println("Heterometrik sayılar: ");      
-        for (int i = 1; i < terim_sayisi; i++) {
+        for (int i = 1; i < terim; i++) {
             System.out.println(+i+(i+1)+(i*(i+1)));
         }
     }

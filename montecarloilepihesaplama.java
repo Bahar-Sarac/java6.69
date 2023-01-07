9b25c10d53c7eaf3145831897ed3c324e148113a
Bahar-Sarac <117797408+Bahar-Sarac@users.noreply.github.com>
2023-01-07 14:13:43 +0300

    Update and rename homework079.java to montecarloilepihesaplama.java

--- montecarloilepihesaplama.java
+++ montecarloilepihesaplama.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
  *
  * @author Bahar
  */
-public class homework079 {
+public class montecarloilepihesaplama {
 
     /**
      * @param args the command line arguments
@@ -14,9 +14,9 @@ public class homework079 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
       
-        double m = 0;
-        double x;
-        double y;
+        double a = 0;
+        double b;
+        double c;
         double max = 0;
         double sonuc;
         
@@ -24,13 +24,13 @@ public class homework079 {
         System.out.print("Bir pozitif tam sayı giriniz: ");
         int n = input.nextInt();
         for (int i = 1; i < n; i++) {
-           x = (Double) (2*Math.random()*(max)/(max-1));
-           y = (Double) (2*Math.random()*(max)/(max-1));
+           x = (Double) (2 * Math.random() * (max) / (max-1));
+           y = (Double) (2 * Math.random() * (max) / (max-1));
            
-            if((Math.pow(x, 2)+Math.pow(y, 2))<1);
-             m++;
+            if((Math.pow(b, 2) + Math.pow(c, 2)) < 1);
+             a++;
         }
-        sonuc = 4*m/n;
+        sonuc = 4*a/n;
                 System.out.println("Monte Carlo yöntemi ile hesaplanan pi sayısı: "+sonuc);
         
     }

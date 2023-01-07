49bb6a4af46fab118d47010369ffc6e01b0e3ba9
Bahar-Sarac <117797408+Bahar-Sarac@users.noreply.github.com>
2023-01-07 14:01:09 +0300

    Update and rename homework072.java to sayibulma.java

--- sayibulma.java
+++ sayibulma.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
  *
  * @author Bahar
  */
-public class homework072 {
+public class sayibulma {
 
     /**
      * @param args the command line arguments
@@ -14,18 +14,18 @@ public class homework072 {
         int min = 1;
         
         Scanner input = new Scanner(System.in);
-        int randomsayi = (int) (Math.random()) * (max - min - 1);
+        int rsayi = (int) (Math.random()) * (max - min - 1);
         int deneme = 0;
         
         while (true) {
             System.out.print("lütfen 1 99 arası sayı giriniz: ");
             int sayi = input.nextInt();
             deneme++;
-            if (sayi > randomsayi) {
+            if (sayi > rsayi) {
                 System.out.println("Lütfen daha küçük bir sayı giriniz: ");
-            } else if (randomsayi > sayi) {
+            } else if (rsayi > sayi) {
                 System.out.println("Lütfen daha büyük bir sayı giriniz: ");
-            } else if (randomsayi == sayi) {
+            } else if (rsayi == sayi) {
                 System.out.print("Tebrikler doğru tahmin: ");
                 break;
             }

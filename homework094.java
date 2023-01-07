a85fc8a9029f7d17467f87e670b5078e4446e630
Bahar-Sarac <117797408+Bahar-Sarac@users.noreply.github.com>
2023-01-07 14:46:03 +0300

    Update homework094.java

--- homework094.java
+++ homework094.java
@@ -15,14 +15,14 @@ public class homework094 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
          
-        int a = 0;
-        System.out.print("Terim sayısı: ");
+        int x = 0;
+        System.out.print("Terim sayısı giriniz: ");
             int n = input.nextInt();
                          
         System.out.println("Fermat sayıları: "); 
          for (int i = 0; i < n-1; i++) {
-            a = (int) Math.pow(2,(Math.pow(2, i)+1));
-             System.out.println(+a); 
+            x = (int) Math.pow(2,(Math.pow(2, i)+1));
+             System.out.println(+x); 
          }
     }
     

ae73794834b31f8ebd7cd18b6ebd9606bac3b61b
Bahar-Sarac <117797408+Bahar-Sarac@users.noreply.github.com>
2023-01-07 15:46:33 +0300

    Create fibonacciterimleri.java

---
+++ fibonacciterimleri.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author Bahar
+ */
+public class fibonacciterimleri {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Terim sayısı giriniz: ");
+        int n = input.nextInt(); 
+        int f1 = 1;
+        int f2 = -1;
+        System.out.print("negafibonacci sayıları: ");
+        System.out.print(f1);
+        System.out.print(f2);
+      
+        for (int i = 1; i <= n-2; i++) {
+            int f3 = f1 - f2;
+            System.out.println(f3);
+            f1 = f2;
+            f2 = f3;
+        }
+    }
+    
+}

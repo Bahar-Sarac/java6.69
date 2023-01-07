0b8665eeab668a1e1f0af30601169d1d411661f7
Bahar-Sarac <117797408+Bahar-Sarac@users.noreply.github.com>
2023-01-07 15:10:28 +0300

    Update and rename homework105.java to aritmetiksayi.java

--- aritmetiksayi.java
+++ aritmetiksayi.java
@@ -6,29 +6,30 @@ import java.util.Scanner;
  *
  * @author Bahar
  */
-public class homework105 {
+public class aritmetiksayi {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        
         System.out.print("Bir tamsayı giriniz:");
         int sayi1 = input.nextInt();
         int t = 0;
-        int s = 0;
+        int sayac = 0;
         for (int i = 1; i <= sayi1; i++) {
             if (sayi1%i == 0) {
                 t += i;
-                s++;
+                sayac++;
             }
         }
         int sayi2 = t/s;
         int tamsayi;
         tamsayi = (int) sayi2;
-        double tamsayıcevirme;
-        tamsayıcevirme = (double) tamsayi;       
-        if (sayi2-tamsayıcevirme == 0) {
+        double tamsayi_cevirme;
+        tamsayi_cevirme = (double) tamsayi;       
+        if (sayi2-tamsayi_cevirme == 0) {
             System.out.println("Aritmetik sayıdır");
         }
         else {

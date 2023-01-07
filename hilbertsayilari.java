79a0271e784d4081b8dfe8ad9de8cdc75cbc0e7a
Bahar-Sarac <117797408+Bahar-Sarac@users.noreply.github.com>
2023-01-07 14:49:02 +0300

    Update hilbertsayilari.java

--- hilbertsayilari.java
+++ hilbertsayilari.java
@@ -14,14 +14,14 @@ public class hilbertsayilari {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
-        System.out.print("Pozitif tamsayı giriniz:");
+        System.out.print("Pozitif tamsayı giriniz: ");
         int s = input.nextInt();
         
         if ((s-1) % 4 == 0) {
-            System.out.println(s+" hilbert sayısıdır");
+            System.out.println(s+" hilbert sayısıdır".);
         }
         else if ((s-1) % 4 != 0) {
-            System.out.println(s+" hilbert sayısı değildir");
+            System.out.println(s+" hilbert sayısı değildir.");
         }
     }
     

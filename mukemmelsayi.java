490d5819c4b8b44293280a8eeec32751eb5f78c1
Bahar-Sarac <117797408+Bahar-Sarac@users.noreply.github.com>
2023-01-07 14:40:29 +0300

    Update and rename homework091.java to mukemmelsayi.java

--- mukemmelsayi.java
+++ mukemmelsayi.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  *
  * @author Bahar
  */
-public class homework091 {
+public class mukemmelsayi {
 
     /**
      * @param args the command line arguments
@@ -15,21 +15,21 @@ public class homework091 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
        
-         System.out.print("Lütfen bir tamsayı giriniz:");
+         System.out.print("Bir tamsayı giriniz:");
          int s = input.nextInt();
          int t = 0;
          for (int i = 1; i < s-1; i++) {
              if(s%i == 0){
                  t += i;
              }
-             else if(s%i != 0){
+             else if(s % i != 0){
              }
          }
           if(s == t){
-              System.out.println(s+" Mükemmel sayıdır");
+              System.out.println(s+" mükemmel sayıdır");
           }
           else if(s != t){
-             System.out.println(s+" Mükemmel sayı değildir"); 
+             System.out.println(s+" mükemmel sayı değildir"); 
           }
         
     }

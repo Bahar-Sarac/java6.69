d117b3bb1c2ce02d4b7dbcda1d59cdd2c1c17083
Bahar-Sarac <117797408+Bahar-Sarac@users.noreply.github.com>
2023-01-07 14:06:58 +0300

    Update and rename homework076.java to brothersformulu.java

--- brothersformulu.java
+++ brothersformulu.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
  *
  * @author Bahar
  */
-public class homework076 {
+public class brothersformulu {
 
     /**
      * @param args the command line arguments
@@ -16,15 +16,15 @@ public class homework076 {
        
        System.out.print("N: ");
        int N = input.nextInt();
-       double toplam = 0;
+       double t = 0;
        for (int i = 2; i <= (N-2)/2; i+=2) {
            int fakt = 1;
            for (int j = 1; j <= i; j++) {
                fakt *= j;
            }
-            toplam += (double) i/fakt;
+            t += (double) i/fakt;
        }
-    System.out.println("Toplam: "+toplam);
+    System.out.println("Toplam: "+t);
     }
     
 }

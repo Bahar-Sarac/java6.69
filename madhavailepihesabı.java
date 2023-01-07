b6b650ea726cffc0e3a8f5b9bcd478c5e10ff43e
Bahar-Sarac <117797408+Bahar-Sarac@users.noreply.github.com>
2023-01-07 14:09:07 +0300

    Update and rename homework077.java to madhavailepihesabı.java

--- madhavailepihesabı.java
+++ madhavailepihesabı.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
  *
  * @author Bahar
  */
-public class homework077 {
+public class madhavailepihesabı {
 
     /**
      * @param args the command line arguments
@@ -15,10 +15,10 @@ public class homework077 {
      Scanner input = new Scanner (System.in);
      
      System.out.print("Terim sayısı giriniz: ");
-     int terim = input.nextInt();
+     int n = input.nextInt();
      double t = 0;
      
-        for (int i = 0; i < terim-1; i++) {
+        for (int i = 0; i < n-1; i++) {
             t += Math.pow(-1, i) / ((2*i+1)* Math.pow(3 ,i));
         }
         System.out.println("Sonuç: "+t);
